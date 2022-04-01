14796fe188f5f0d5b559bcbf3f7b706a8561533b
Loc <daoloc93@gmail.com>
2022-04-01 16:19:53 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle("orange", true, 2.5, 3.8);
+        System.out.println(rectangle);
+
+        Square square = new Square();
+        System.out.println(square);
+
+        square = new Square(2.3);
+        System.out.println(square);
+
+        square = new Square(5.8, "yellow", true);
+        System.out.println(square);
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,55 @@
+public class Rectangle extends Shape {
+
+    double width;
+    double length;
+
+    public Rectangle() {
+        this.width = 1;
+        this.length= 1;
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (this.width + this.length);
+    }
+
+    @Override
+    public String toString() {
+        return "A Rectangle with width="
+                + getWidth()
+                + " and length="
+                + getLength()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
